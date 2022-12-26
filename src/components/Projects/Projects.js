c088eabd7b9d3d121809b3c41cd4abfb67c0f0a6
suleymanlaarabidev <suleymanlaarabi01@gmail.com>
2022-12-26 16:31:12 +0100

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import SulyMusicplayer from "../../Assets/Suly-music-banniere.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={SulyMusicplayer}
+              isBlog={false}
+              title="Suly Music Player"
+              description="Material You Design music player for Android"
+              ghLink="https://github.com/suleymanlaarabidev/Suly-Music"
+              demoLink="https://t.me/SulyMusicPlayer"
+            />
+          </Col>
+
+
+
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
