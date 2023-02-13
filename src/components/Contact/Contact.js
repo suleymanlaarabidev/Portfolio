671f6b47993fe5858531f457763e1d513378e187
suleymanlaarabidev <suleymanlaarabi01@gmail.com>
2023-02-13 14:39:11 +0100

    fixe mail sender in page contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import Particle from '../Particle';
-
+import axios from "axios"
 const Contact = () => {
+
+    const firstNameRef = useRef()
+    const lastNameRef = useRef()
+
+    const mailRef = useRef()
+    const objectRef = useRef()
+    const messageRef = useRef()
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        axios.get(`http://82.64.55.123:8080/sendmail/${firstNameRef.current.value}/${lastNameRef.current.value}/${mailRef.current.value}/${objectRef.current.value}/${messageRef.current.value}`).then(res => {
+            console.log(res)
+        })
+
+
     }
+
     return (
         <Container fluid className="contact-section">
             <Particle />
@@ -14,13 +28,14 @@ const Contact = () => {
                 <h1>LET’S <span style={{ color: "#8a49a8" }}>TALK</span></h1>
                 <p>Fill in the form below, or reach out to us directly — <span style={{ color: "#8a49a8" }}>suleyman.laarabi.dev@gmail.com</span> </p>
                 <div className='name-input'>
-                    <input type="text" placeholder='First Name' />
-                    <input type="text" placeholder='Last Name' />
+                    <input required ref={firstNameRef} type="text" placeholder='First Name' />
+                    <input required ref={lastNameRef} type="text" placeholder='Last Name' />
                 </div>
 
-                <input type="text" placeholder='Mail' />
-                <input type="text" placeholder='Object' />
-                <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+                <input required ref={mailRef} type="email" placeholder='Mail' />
+                <input required ref={objectRef} type="text" placeholder='Object' />
+                <textarea required ref={messageRef} name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+                <button>Envoyer</button>
             </form>
         </Container>
     );
